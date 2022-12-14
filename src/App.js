2bb09930bdc60fa92438c45829d71fbb81ab82e7
nithishkk <109412506+nithishkk@users.noreply.github.com>
2022-12-14 01:12:39 -0500

    addeed usng event

--- src/App.js
+++ src/App.js
@@ -12,11 +12,17 @@ const App=()=>{
     console.log(e.target.value)
     // console.log(data)
   }
+  // const handleChange = (e, name, value, key) => {
+  //   setData((prevState) => ({
+  //       ...prevState,
+  //       [name]: value
+  //   }))
   // console.log(e.target.value)
   return(
     <div>
       name :<input id='name' type='text'  value={data.name} onChange={nithis}/> <br/>
-      email:<input type='text' name='email' value={data.email} onChange={nithis}/>
+      {/* email:<input type='text' name='email' value={data.email} onChange ={(e,value)=>handleChange(event,"email",value,"email")}/> */}
+      email :<input id='name' type='text'  value={data.email} onChange={nithis}/> <br/>
       </div>
   )
 }
